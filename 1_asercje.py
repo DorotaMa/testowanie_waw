cefbcf15b62084962890dc56d8a9bc392eb22398
Your Name <you@example.com>
2019-12-15 11:23:42 +0100

    Pierwszy test

---
+++ 1_asercje.py
@@ -0,0 +1,15 @@
+def dzielenie(a,b):
+    return a/b
+
+
+if dzielenie(10, 5) ==2:
+    print("Passed")
+else:
+    print("Filed")
+
+if dzielenie(10, 2) ==5:
+    print("Passed")
+else:
+    print("Filed")
+
+print(dzielenie(10,1)==2)
